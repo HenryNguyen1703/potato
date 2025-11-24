fad9c3dfb59d581a8c9c3deb26e8c36c8b4881e9
tanpuh <3122411191@sv.sgu.edu.vn>
2025-11-24 21:20:52 +0700

    public drone endpoints (remove)

--- potato-api/src/main/java/com/ktpm/potatoapi/common/config/security/SecurityConfig.java
+++ potato-api/src/main/java/com/ktpm/potatoapi/common/config/security/SecurityConfig.java
@@ -34,7 +34,6 @@ public class SecurityConfig {
             "/merchants/{merchantId}/categories", "/merchants", "/merchants/{id}",
             "/menu-items/{menuItemId}", "/merchants/{merchantId}/menu-items",
             "/merchants", "/merchants/{id}",
-            "/admin/drones/{id}/update-location", "/admin/drones/{id}"
     };
     String[] CUSTOMER_ENDPOINTS = {
             "/cart",
